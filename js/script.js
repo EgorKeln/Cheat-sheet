fd9f4a06b670e962ff5d3705676c7a91421ff1cb
Keln <122953267+EgorKeln@users.noreply.github.com>
2023-02-20 22:38:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function selectEdge(){
+    const cube = document.querySelector('.cube');
+    const degs = { 
+        back:   {X:-180, Y: 0,},
+        top:    {X: 0, Y: 0},
+        front:  {X:-90, Y: 0},
+        bottom: {X: 90, Y: 0},
+        left:   {X: 0, Y: 90},
+        right:  {X: 0, Y:-90},
+    }
+
+    document.querySelectorAll('nav a').forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            e.preventDefault();
+            let edgeName = e.target.hash.replace(/#/, '');
+            cube.style.transform = 'perspective(300px) rotateX('+degs[edgeName].X+'deg) rotateY('+degs[edgeName].Y+'deg)';
+        })
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    selectEdge();
+});
+
+
+
+
+
+
+/*
+function selectEdge(){
+    const cube = document.querySelector('.cube');
+    const degs = { 
+        back:   {X:-180, Y: 0,},
+        top:    {X:-90, Y: 0},
+        front:  {X: 0, Y: 0},
+        bottom: {X: 90, Y: 0},
+        left:   {X: 0, Y: 90},
+        right:  {X: 0, Y:-90},
+    }
+
+    document.querySelectorAll('nav a').forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            e.preventDefault();
+            let edgeName = e.target.hash.replace(/#/, '');
+            cube.style.transform = 'perspective(1150px) rotateX('+degs[edgeName].X+'deg) rotateY('+degs[edgeName].Y+'deg)';
+        })
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    selectEdge();
+});
+*/
