4b0085ab2a274cfac9622313ab3bd7978aea95a7
Keln <122953267+EgorKeln@users.noreply.github.com>
2023-02-21 23:18:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+/* ============================================================== кубик */
+function selectEdge(){
+    const cube = document.querySelector('.nav-cube');
+    const degs = { 
+        back:   {X:-180, Y: 0,},
+        top:    {X: 0, Y: 0},
+        front:  {X:-90, Y: 0},
+        bottom: {X: 90, Y: 0},
+        left:   {X: 0, Y: 90},
+        right:  {X: 0, Y:-90},
+    }
+
+    document.querySelectorAll('nav').forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            e.preventDefault();
+            let edgeName = e.target.hash.replace(/#/, '');
+            cube.style.transform = 'perspective(700px) rotateX('+degs[edgeName].X+'deg) rotateY('+degs[edgeName].Y+'deg)';
+        })
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    selectEdge();
+});
+
+/* ============================================================== менюшка */
+const list = document.querySelectorAll('.nav-menu-list');
+    function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+     }
+    list.forEach((item) =>
+    item.addEventListener('click',activeLink));
+
+
+
+
+
+
+
+
+/* ============================================================== кубик 
 function selectEdge(){
     const cube = document.querySelector('.cube');
     const degs = { 
@@ -9,7 +52,7 @@ function selectEdge(){
         right:  {X: 0, Y:-90},
     }
 
-    document.querySelectorAll('nav a').forEach(elem => {
+    document.querySelectorAll('.nav-ling').forEach(elem => {
         elem.addEventListener('click', (e) => {
             e.preventDefault();
             let edgeName = e.target.hash.replace(/#/, '');
@@ -20,35 +63,15 @@ function selectEdge(){
 
 document.addEventListener('DOMContentLoaded', () => {
     selectEdge();
-});
+});*/
 
-
-
-
-
-
-/*
-function selectEdge(){
-    const cube = document.querySelector('.cube');
-    const degs = { 
-        back:   {X:-180, Y: 0,},
-        top:    {X:-90, Y: 0},
-        front:  {X: 0, Y: 0},
-        bottom: {X: 90, Y: 0},
-        left:   {X: 0, Y: 90},
-        right:  {X: 0, Y:-90},
-    }
-
-    document.querySelectorAll('nav a').forEach(elem => {
-        elem.addEventListener('click', (e) => {
-            e.preventDefault();
-            let edgeName = e.target.hash.replace(/#/, '');
-            cube.style.transform = 'perspective(1150px) rotateX('+degs[edgeName].X+'deg) rotateY('+degs[edgeName].Y+'deg)';
-        })
-    });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    selectEdge();
-});
+/* ============================================================== менюшка 
+const list = document.querySelectorAll('.list');
+    function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+     }
+    list.forEach((item) =>
+    item.addEventListener('click',activeLink));
 */
